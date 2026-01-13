409a14571dbd209906e80300e52e8a96f5ad80d5
冬桜子 濱元 <toko0124615@icloud.com>
2026-01-13 12:31:03 +0900

    a

--- ConsoleApp9/Program.cs
+++ ConsoleApp9/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Numerics;
+
+class Program 
+{
+
+    static void Main(string[] args) 
+    {
+
+        int sum = 0;
+        int inputNumber = 0;
+
+        while (sum <= 100)
+        {
+            Console.WriteLine("数字を入力してください: ");
+            string input = Console.ReadLine();
+            int number = int.Parse(input);
+
+            if (int.TryParse(input, out inputNumber))
+            {
+
+                sum += inputNumber;
+            }
+     
+        }
+        {
+            Console.WriteLine($"合計は{sum}です");
+        }
+        }
+    
+    
+    
+    }
+
+
